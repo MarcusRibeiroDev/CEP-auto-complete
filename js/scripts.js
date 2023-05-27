5453fc293410a4dad24f05598753f5cf7b08a34d
MarcusRibeiroDev <marcus10dev@gmail.com>
2023-05-27 14:41:05 -0300

    Estudos

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const addressForm = document.querySelector("#address-form")
+const cepInput = document.querySelector("#cep")
+const streetInput = document.querySelector("#address")
+const cityInput = document.querySelector("#city")
+const neighbordhoodInput = document.querySelector("#neighborhood")
+const stateInput = document.querySelector("#region")
+const formInputs = document.querySelectorAll("[data-input]")
+
+const closeButton = document.querySelector("close-message")
